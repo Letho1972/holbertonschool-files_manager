5f8652b8d976dfcdb2dbf8c8c67f30f71534a226
Letho1972 <ericmarescq@gmail.com>
2024-08-02 15:45:47 +0200

    fixed redis

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,30 +1,46 @@
-import { createClient } from 'redis';
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
+    this.client = redis.createClient();
     this.client.on('error', (error) => {
-      console.error(`Redis client error: ${error}`);
+      console.log(error);
     });
-    this.client.connect();
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    return this.client.get(key);
+    try {
+      const value = await this.getAsync(key);
+      return (value);
+    } catch (err) {
+      console.error('Error fetching value:', err);
+      return null;
+    }
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, {
-      EX: duration
-    });
+    try {
+      await this.setAsync(key, value, 'EX', duration);
+    } catch (err) {
+      console.error('Error setting value:', err);
+    }
   }
 
   async del(key) {
-    await this.client.del(key);
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.error('Error deleting value:', err);
+    }
   }
 }
 
