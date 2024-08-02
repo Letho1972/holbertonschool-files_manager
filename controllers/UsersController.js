abb4ff136bbc48870c7114f7e6ed3f1f3e9bc487
Letho1972 <ericmarescq@gmail.com>
2024-08-02 18:12:40 +0200

    3. Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,47 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      res.status(400);
+      return res.json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      res.status(400);
+      return res.json({ error: 'Missing password' });
+    }
+
+    const exist = await dbClient.doesUserExist(email);
+    if (exist) {
+      res.status(400);
+      return res.json({ error: 'Already exist' });
+    }
+
+    const id = await dbClient.createUser(email, password);
+    res.status(201);
+    return res.json({ id, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const tokenKey = `auth_${token}`;
+    const userId = await redisClient.get(tokenKey);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const user = await dbClient.findUserById(userId);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
+}
+
+export default UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+const app = express.Router();
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+app.use(express.json());
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+app.get('/status', AppController.getStatus);
+app.get('/stats', AppController.getStats);
+app.post('/users', UsersController.postNew);
+app.get('/connect', AuthController.getConnect);
+app.get('/disconnect', AuthController.getDisconnect);
+app.get('/users/me', UsersController.getMe);
+app.post('/files', FilesController.postUpload);
+app.put('/files/:id/publish', FilesController.putPublish);
+app.put('/files/:id/unpublish', FilesController.putUnpublish);
+app.get('/files/:id', FilesController.getShow);
+app.get('/files', FilesController.getIndex);
+app.get('/files/:id/data', FilesController.getFile);
 
-}
-
-export default controllerRouting;
+export default app;
